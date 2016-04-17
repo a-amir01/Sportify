fe4dfb4bae81d32424c3c65a07f9fc7b2e2cc904
Amir Assad <amirassad@protonmail.ch>
2016-04-17 13:11:31 -0700

    initial commit

--- app/src/main/java/gspot/com/sportify/Controller/loginActivity.java
+++ app/src/main/java/gspot/com/sportify/Controller/loginActivity.java
@@ -1,5 +1,6 @@
 package gspot.com.sportify.Controller;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,10 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
 
 import org.w3c.dom.Text;
 
@@ -24,15 +29,29 @@ public class loginActivity extends AppCompatActivity {
 
     private static final String TAG = loginActivity.class.getSimpleName();
 
+    /* A reference to the Firebase */
+    private Firebase mFirebaseRef;
+
+    /* Data from the authenticated user */
+    private AuthData mAuthData;
+
+    /* Listener for Firebase session changes */
+    private Firebase.AuthStateListener mAuthStateListener;
+
+
+    /*link to the widgets*/
     @Bind(R.id.input_email) EditText mEmailText;
     @Bind(R.id.input_password) EditText mPasswordText;
     @Bind(R.id.btn_login) Button mLoginButton;
     @Bind(R.id.link_signup) TextView mSignupText;
 
-    @OnClick(R.id.input_email)
-    void onClick(EditText text) {
-
-    }
+    /* onClick
+     * Annotation listener for the login button
+     * Once the button is clicked the login() is called
+     * to log the user in
+     * */
+    @OnClick(R.id.btn_login)
+    void onClick(Button button) { login(); }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +63,91 @@ public class loginActivity extends AppCompatActivity {
         /*link the widgets to the members*/
         ButterKnife.bind(this);
 
-
-
     }
 
+
+    private void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        mLoginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(loginActivity.this,
+                R.style.AppTheme);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = mEmailText.getText().toString();
+        String password = mPasswordText.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    /*
+    * validate()
+    * This utility function will check and make sure that the
+    * user has entered the correct email and password syntax
+    * */
+    private boolean validate() {
+        Log.i(TAG, "validate()");
+
+        boolean valid = true;
+
+        String email = mEmailText.getText().toString();
+        String password = mPasswordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmailText.setError("enter a valid email address");
+            valid = false;
+        } //end if
+        else {
+            mEmailText.setError(null);
+        } //end else
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            mPasswordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } //end if
+        else {
+            mPasswordText.setError(null);
+        } //end else
+
+        return valid;
+    }/*end validate()*/
+
+    /*
+    * onLoginSuccess()
+    * re enables the login button and closes the activity
+    * */
+    public void onLoginSuccess() {
+        mLoginButton.setEnabled(true);
+        finish();
+    }
+
+    /*
+    * onLoginFailed()
+    * Function to notify user of a failed login
+    * reenables the login button
+    * */
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        mLoginButton.setEnabled(true);
+    }
 
 }
---
+++ app/src/main/java/gspot/com/sportify/Model/user.java
@@ -0,0 +1,7 @@
+package gspot.com.sportify.Model;
+
+/**
+ * Created by amir on 4/17/16.
+ */
+public class user {
+}
