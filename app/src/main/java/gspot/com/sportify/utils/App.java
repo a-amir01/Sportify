83eb515140c30929449b67785d25b212f2089985
yunfanyang(andrew) <andrewyang1995@gmail.com>
2016-05-15 15:46:16 -0700

    added delete to ViewEvents

--- app/src/main/java/gspot/com/sportify/utils/App.java
+++ app/src/main/java/gspot/com/sportify/utils/App.java
@@ -11,6 +11,9 @@ import gspot.com.sportify.Model.Gathering;
  * Created by yunfanyang on 5/2/16.
  */
 public class App {
-    public static Firebase dbref = new Firebase("https://sporttesttt.firebaseio.com");
+    public static Firebase dbref = new Firebase("https://gspot.firebaseio.com");
     public static List<Gathering> mGatherings = new ArrayList<>();
+    /* current gathering selected*/
+    public static Gathering mCurrentGathering = null;
+
 }
