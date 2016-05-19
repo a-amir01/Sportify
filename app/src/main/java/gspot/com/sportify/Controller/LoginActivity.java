c1398ab8d2a5ff1be9a3635a21808593d0d1e062
Harsh Patel <harshpatel081296@gmail.com>
2016-05-18 17:34:34 -0700

    (updated Login page mainly and other small functionality)

--- app/src/main/java/gspot/com/sportify/Controller/LoginActivity.java
+++ app/src/main/java/gspot/com/sportify/Controller/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
