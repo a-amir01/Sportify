465c474726267b09c6320475acb913f984730a5d
armin-ahmadi <armin.ahmadi4@gmail.com>
2016-05-29 12:35:33 -0700

    Updated fragment_gathering.xml layout (for creating/updating gatherings). New layout is saved in fragment_gathering_update.xml

--- app/src/main/java/gspot/com/sportify/Controller/AddSportFragment.java
+++ app/src/main/java/gspot/com/sportify/Controller/AddSportFragment.java
@@ -1,44 +1,27 @@
 package gspot.com.sportify.Controller;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.firebase.client.Firebase;
-import com.firebase.client.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnCheckedChanged;
 import gspot.com.sportify.Model.MySport;
 import gspot.com.sportify.Model.Profile;
 import gspot.com.sportify.Model.SportTypes;
 import gspot.com.sportify.R;
-import gspot.com.sportify.utils.Constants;
 
 /** Add Sport Fragment Class
  * Represents a list of sports for the user to
@@ -78,6 +61,7 @@ public class AddSportFragment extends DialogFragment {
 
         Resources res = getResources();
         TextView title = (TextView) getDialog().findViewById(android.R.id.title);
+        Log.i(TAG, (String) title.getText());
         title.setText("Choose a sport");
         title.setTextColor( res.getColor(R.color.colorPrimaryText));
 
